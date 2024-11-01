bff090cfdc17d6d64348e3cd8d59a36818fad685
Arigayu <153355908+Arigayuu@users.noreply.github.com>
2024-11-01 16:28:29 +0800

    Add files via upload

--- Quiz1/searcing.py
+++ Quiz1/searcing.py
@@ -5,10 +5,12 @@ x = [3, 6, 8, 11, 14, 17, 20, 23, 26, 29]
 y = int(input("Masukkan angka untuk y1: "))
 
 # Logika untuk mengecek keberadaan y1 di dalam x
-if  y in x:
+if y in x:
+    index = x.index(y)  # Mencari indeks di mana y ditemukan dalam array
     if y % 2 == 0:
-        print(f"{y} adalah genap dan ada di dalam array.")
+        print(f"{y} adalah genap dan ada di dalam array pada indeks {index}.")
     else:
-        print(f"{y} ada di dalam array, tetapi bukan genap.")
+        print(f"{y} ada di dalam array pada indeks {index}, tetapi bukan genap.")
 else:
     print("Variabel tidak ada dalam array.")
+6
